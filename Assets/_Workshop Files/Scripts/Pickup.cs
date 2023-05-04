2fb97d5986d7b0e51d65db607e1d6779169d6e2f
Bora Ilkinonu <borailkinonu@gmail.com>
2023-05-04 17:51:48 +0300

    Initial commit

---
+++ Assets/_Workshop Files/Scripts/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace _Complete
+{
+    public class Pickup : MonoBehaviour
+    {
+
+        private int coinCount;
+        public TextMeshProUGUI coinText;
+        public AudioClip coinSound;
+        public AudioSource pickupAudioSource;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Coin"))
+            {
+                // AudioSource.PlayClipAtPoint(coinSound, other.transform.position);
+                pickupAudioSource.clip = coinSound;
+                pickupAudioSource.Play();
+                coinCount++;
+                coinText.text = coinCount.ToString();
+                Destroy(other.gameObject);
+
+            }
+        }
+        
+        
+
+    }
+}
